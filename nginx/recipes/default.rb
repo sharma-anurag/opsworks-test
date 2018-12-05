0fe8b0fdc1e22ae6f5df60e4917e56cb40ad1c5d
Anurag Sharma <anurag@ACKO228.local>
2018-12-05 17:03:38 +0530

    Test Nginx

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -1,3 +1,10 @@
 package 'nginx' do
-	action :install
+  action :install
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
 end
